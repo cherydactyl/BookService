c0f17be261eb4153b84a4645f19d5f93db51c7ca
cherydactyl <cheryl.orosz@gmail.com>
2015-08-03 16:18:27 -0400

    Removed (commented out) circular reference demo code

--- BookService/Models/Author.cs
+++ BookService/Models/Author.cs
@@ -8,5 +8,9 @@ namespace BookService.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+
+        //Circular reference demonstration
+        //public ICollection<Book> Books { get; set; }
+
     }
 }

82efd39bc7e79db68bf87eec163e4d83d56ced31
cherydactyl <cheryl.orosz@gmail.com>
2015-08-03 16:00:47 -0400

    Added using statement to Configuration.cs file
    http://www.asp.net/web-api/overview/data/using-web-api-with-entity-framework/part-3

--- BookService/Migrations/Configuration.cs
+++ BookService/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace BookService.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using BookService.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BookService.Models.BookServiceContext>
     {

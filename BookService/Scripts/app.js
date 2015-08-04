8213448a6cac77f99c7d8fe75c281e9d30358d0c
cherydactyl <cheryl.orosz@gmail.com>
2015-08-04 14:12:58 -0400

    Details working

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -2,6 +2,7 @@
     var self = this;
     self.books = ko.observableArray();
     self.error = ko.observable();
+    self.detail = ko.observable();
 
     var booksUri = '/api/books/';
 
@@ -24,6 +25,11 @@
         });
     }
 
+    self.getBookDetail = function (item) {
+        ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
+            self.detail(data);
+        });
+    }
     // Fetch the initial data.
     getAllBooks();
 };
